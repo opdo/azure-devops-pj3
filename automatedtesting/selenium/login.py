b6cf9232e5524ee48bd857ef860ce73f36508e76
Vinh Pham <pham.vinh1995@gmail.com>
2023-07-18 11:34:31 +0700

    Fix log

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -12,6 +12,7 @@ def start ():
     options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
+    options.add_argument("--remote-debugging-port=9222")
     
     driver = webdriver.Chrome(options=options)
     # driver = webdriver.Chrome()
