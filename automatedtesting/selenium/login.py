912fdfc1a567e27a3b38b9512acafbf862ab2060
Vinh Pham <pham.vinh1995@gmail.com>
2023-07-18 11:01:50 +0700

    Fix selenium

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -8,7 +8,8 @@ def start ():
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
-    options.add_argument("--headless") 
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
     # driver = webdriver.Chrome()
 
