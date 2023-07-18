9957b05c3235ee432e2feecd40768e75d6c58f13
Vinh Pham <pham.vinh1995@gmail.com>
2023-07-18 11:42:39 +0700

    add timestamp

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -2,10 +2,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
+import datetime
 
 # Start the browser and perform the test
 def start ():
-    print ('Starting the browser...')
+    print (timestamp() + 'Starting the browser...')
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
     
@@ -13,7 +14,7 @@ def start ():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
     options.add_argument("--remote-debugging-port=9222")
-    
+
     driver = webdriver.Chrome(options=options)
     # driver = webdriver.Chrome()
 
@@ -28,50 +29,54 @@ def start ():
 
 # Login method
 def login (driver, user, password):
-    print ('Browser started successfully. Navigating to the demo page to login.')
+    print (timestamp() + 'Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
 
-    print ('Input username')
+    print (timestamp() + 'Input username')
     driver.find_element(By.CSS_SELECTOR, "input[id = 'user-name']").send_keys(user)
 
-    print ('Input password')
+    print (timestamp() + 'Input password')
     driver.find_element(By.CSS_SELECTOR, "input[id = 'password']").send_keys(password)
 
-    print ('Login')
+    print (timestamp() + 'Login')
     driver.find_element(By.CSS_SELECTOR, "input[id = 'login-button']").click()
 
-    print ('Check login success or not')
+    print (timestamp() + 'Check login success or not')
     logoElements = driver.find_elements(By.CSS_SELECTOR, ".app_logo")
     assert len(logoElements) > 0, "Element not found"
 
 # Add cart
 def add_cart(driver):
-    print ('Add all product')
+    print (timestamp() + 'Add all product')
     productElements = driver.find_elements(By.CSS_SELECTOR, ".inventory_item")
 
     for product in productElements:
         productButton = product.find_element(By.CSS_SELECTOR, ".btn_inventory")
         productName = product.find_element(By.CSS_SELECTOR, ".inventory_item_name")
 
-        print(f"Add product name to {productName.text} cart")
+        print(timestamp() + f"Add product name to {productName.text} cart")
         productButton.click()
 
-    print('Verify cart count')
+    print(timestamp() + 'Verify cart count')
     cartCount = int(driver.find_element(By.CSS_SELECTOR, ".shopping_cart_badge").text)
     assert cartCount == len(productElements), 'The cart count does not correct'
 
 # Remove all product
 def remove_cart(driver):
-    print ('Go to cart page')
+    print (timestamp() + 'Go to cart page')
     driver.find_element(By.CSS_SELECTOR, ".shopping_cart_link").click()
 
-    print ('Remove one by one product')
+    print (timestamp() + 'Remove one by one product')
     removeButtons = driver.find_elements(By.CSS_SELECTOR, ".cart_button")
     for remove in removeButtons:
         remove.click()
 
-    print('Verify remove success or not')
+    print(timestamp() + 'Verify remove success or not')
     cartCountElement = driver.find_elements(By.CSS_SELECTOR, ".shopping_cart_badge")
     assert len(cartCountElement) == 0, "Remove failed"
+
+def timestamp():
+    ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return (ts + '\t')
 
 start()
